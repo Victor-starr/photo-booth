11393a469a71098de71fe60d73677fd31352a772
Victor-starr <v94625230@gmail.com>
2025-08-14 22:01:20 +0300

    feat: add NavSuggestion component and enhance navigation with profile link

---
+++ src/components/NavSuggestion.tsx
@@ -0,0 +1,39 @@
+import { useCamera } from "@/hook/useCamera";
+import Link from "next/link";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { MdAdsClick } from "react-icons/md";
+
+function NavSuggestion() {
+  const { photosArr } = useCamera();
+  const [visible, setVisible] = useState(true);
+
+  //   TODO make it so if the user clicks on the close button, it will not show again
+  if (!visible || photosArr.length !== 4) {
+    return null;
+  }
+
+  return (
+    <div className="relative flex flex-wrap justify-center items-center gap-5 bg-pink-300 py-2 pr-15 pl-5 text-white text-base md:text-lg lg:text-xl text-center">
+      <IoClose
+        onClick={() => setVisible(false)}
+        className="right-5 lg:right-10 absolute"
+        aria-label="Close"
+        size={25}
+      />
+      <p>We still have your photos from last session you had!</p>
+      <MdAdsClick
+        size={25}
+        className="lg:hidden inline-block flex-shrink-0 text-red-800 hover:text-red-600"
+      />
+      <Link
+        href="/customize"
+        className="hidden lg:flex flex-shrink-0 justify-center items-center text-red-800 hover:text-red-600 underline"
+      >
+        CLICK HERE !!
+      </Link>
+    </div>
+  );
+}
+
+export default NavSuggestion;
--- src/components/_Nav.tsx
+++ src/components/_Nav.tsx
@@ -3,9 +3,10 @@
 import { useAuth } from "@/hook/useAuth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { IoCamera } from "react-icons/io5";
+import { IoCamera, IoPersonSharp } from "react-icons/io5";
 import { LuLogOut, LuLogIn } from "react-icons/lu";
 import { FaGithub } from "react-icons/fa";
+import NavSuggestion from "./NavSuggestion";
 
 const Nav = () => {
   const { isAuthenticated, logout } = useAuth();
@@ -27,43 +28,55 @@ const Nav = () => {
   };
 
   return (
-    <nav className="z-3 flex justify-around bg-pink-2 px-6 md:px-10 py-4 md:py-6 w-full h-auto overflow-hidden">
-      <Link
-        className="relative flex items-center shadow-red-500 h-auto text-cst"
-        href={"/"}
-      >
-        <h2 className="text-2xl md:text-3xl lg:text-4xl">Photo Booth</h2>
-        <IoCamera className="hidden md:inline relative ml-2 size-7 sm:size-8 md:size-9 lg:size-10 rotate-25" />
-      </Link>
-      <div className="flex flex-row flex-shrink-0 items-center gap-2 lg:gap-6">
-        {isAuthenticated ? (
-          <button
-            onClick={() => handleRoute("/logout")}
-            className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 disabled:bg-gray-400 md:px-4 md:py-2 rounded-2xl text-white text-xl md:text-2xl transition-colors duration-200"
-          >
-            <LuLogOut className="md:hidden block size-6" />
-            <span className="hidden md:inline">Log Out</span>
-          </button>
-        ) : (
-          <button
-            onClick={() => handleRoute("/login")}
-            className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 disabled:bg-gray-400 md:px-4 md:py-2 rounded-2xl text-white text-xl md:text-2xl transition-colors duration-200"
-          >
-            <LuLogIn className="md:hidden block size-6" />
-            <span className="hidden md:inline">Log In</span>
-          </button>
-        )}
-
+    <>
+      <nav className="z-3 flex justify-around bg-pink-2 px-6 md:px-10 py-4 md:py-6 w-full h-auto overflow-hidden">
         <Link
-          href="https://github.com/Victor-starr/photo-booth"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 rounded-2xl w-8 sm:w-9 md:w-11 h-8 sm:h-9 md:h-11"
+          className="relative flex items-center shadow-red-500 h-auto text-cst"
+          href={"/"}
         >
-          <FaGithub className="size-6 text-white" />
+          <h2 className="text-2xl md:text-3xl lg:text-4xl">Photo Booth</h2>
+          <IoCamera className="hidden md:inline relative ml-2 size-7 sm:size-8 md:size-9 lg:size-10 rotate-25" />
         </Link>
-      </div>
-    </nav>
+        <div className="flex flex-row flex-shrink-0 items-center gap-2 lg:gap-6">
+          {isAuthenticated ? (
+            <>
+              <Link
+                href="/profile"
+                className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 disabled:bg-gray-400 md:px-4 md:py-2 rounded-2xl text-white text-xl md:text-2xl transition-colors duration-200"
+              >
+                <span className="hidden md:inline">Profile</span>
+                <IoPersonSharp className="md:hidden block size-6" />
+              </Link>
+              <button
+                onClick={() => handleRoute("/logout")}
+                className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 disabled:bg-gray-400 md:px-4 md:py-2 rounded-2xl text-white text-xl md:text-2xl transition-colors duration-200"
+              >
+                <LuLogOut className="md:hidden block size-6" />
+                <span className="hidden md:inline">Log Out</span>
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => handleRoute("/login")}
+              className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 disabled:bg-gray-400 md:px-4 md:py-2 rounded-2xl text-white text-xl md:text-2xl transition-colors duration-200"
+            >
+              <LuLogIn className="md:hidden block size-6" />
+              <span className="hidden md:inline">Log In</span>
+            </button>
+          )}
+
+          <Link
+            href="https://github.com/Victor-starr/photo-booth"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex justify-center items-center bg-pink-400 hover:bg-pink-8 rounded-2xl w-8 sm:w-9 md:w-11 h-8 sm:h-9 md:h-11"
+          >
+            <FaGithub className="size-6 text-white" />
+          </Link>
+        </div>
+      </nav>
+      <NavSuggestion />
+    </>
   );
 };
 
