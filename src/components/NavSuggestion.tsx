0ccf4d70a4f70abdaa835c9f9a7a5e5b2eb2cbeb
Victor-starr <v94625230@gmail.com>
2025-08-15 14:54:02 +0300

    feat: implement useLocalStorageBoolean hook and enhance NavSuggestion with authentication check and add loading state to the navigation component

--- src/components/NavSuggestion.tsx
+++ src/components/NavSuggestion.tsx
@@ -1,20 +1,25 @@
+import { useAuth } from "@/hook/useAuth";
 import { useCamera } from "@/hook/useCamera";
+import useLocalStorageBoolean from "@/hook/useLocalStorageBoolean";
 import Link from "next/link";
-import { useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { MdAdsClick } from "react-icons/md";
+import { redirect } from "next/navigation";
 
 function NavSuggestion() {
   const { photosArr } = useCamera();
-  const [visible, setVisible] = useState(true);
+  const { isAuthenticated } = useAuth();
+  const [visible, setVisible] = useLocalStorageBoolean(
+    "showNavSuggestion",
+    true
+  );
 
-  //   TODO make it so if the user clicks on the close button, it will not show again
-  if (!visible || photosArr.length !== 4) {
+  if (!visible || photosArr.length !== 4 || !isAuthenticated) {
     return null;
   }
 
   return (
-    <div className="relative flex flex-wrap justify-center items-center gap-5 bg-pink-300 py-2 pr-15 pl-5 text-white text-base md:text-lg lg:text-xl text-center">
+    <div className="relative flex flex-row justify-center items-center gap-5 bg-pink-300 py-2 pr-15 pl-5 text-white text-sm md:text-lg lg:text-xl text-center">
       <IoClose
         onClick={() => setVisible(false)}
         className="right-5 lg:right-10 absolute"
@@ -24,11 +29,12 @@ function NavSuggestion() {
       <p>We still have your photos from last session you had!</p>
       <MdAdsClick
         size={25}
-        className="lg:hidden inline-block flex-shrink-0 text-red-800 hover:text-red-600"
+        className="lg:hidden inline-block flex-shrink-0 text-red-800 hover:text-red-600 active:text-red-600"
+        onClick={() => redirect("/customize")}
       />
       <Link
         href="/customize"
-        className="hidden lg:flex flex-shrink-0 justify-center items-center text-red-800 hover:text-red-600 underline"
+        className="hidden lg:flex flex-shrink-0 justify-center items-center text-red-800 hover:text-red-600 active:text-red-600 underline"
       >
         CLICK HERE !!
       </Link>
